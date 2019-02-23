43faf1b5f13230b613425168a50a571253571f1e
Jordan Epps <jordanepps@gmail.com>
2019-02-23 16:25:56 -0500

    Add rating to playstoreapps component

--- src/PlaystoreApp/PlaystoreApp.js
+++ src/PlaystoreApp/PlaystoreApp.js
@@ -5,13 +5,7 @@ export default function PlaystoreApp(props) {
 	return (
 		<div className="playstore-apps">
 			<h2>{props.App}</h2>
-			{/* <div className="book_author">by {props.author}</div> */}
-			{/* <div className="book_publisher">
-				Published by: {props.publisher}
-				on {moment(props.published_date).format('DD MMM YYYY')}
-			</div> */}
-			{/* <div className="book_description">{props.description}</div> */}
-			{/* <div className="book_details">Rank {props.rank} this week</div> */}
+			<h3>Rating: {props.Rating}</h3>
 		</div>
 	);
 }
